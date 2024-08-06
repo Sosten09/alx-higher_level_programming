1c9fd2e1f839a756e8bb819a29d37d3bb7de25a1
Sosten09 <sostensemu09@gmail.com>
2024-08-06 10:00:12 -0700

    correction

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-myvar = "C is fun"
-myvar2 =" Python is cool"
-myvar3 = "JavaScript is amazing"
+const myvar = "C is fun"
+const myvar2 =" Python is cool"
+const myvar3 = "JavaScript is amazing"
 console.log(myvar)
 console.log(myvar2)
 console.log(myvar3)
